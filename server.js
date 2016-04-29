94c535a617339940aacbf9691103756bb79cb83e
MohmadAyman <no.future2e1@gmail.com>
2016-04-29 09:24:30 +0200

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = require('express')();
 var port = process.env.OPENSHIFT_NODEJS_PORT || '8000';
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 app.use(express.static(__dirname + '/'));
 app.all('*', function(req, res, next) {
   res.sendFile('one-page.html', { root: __dirname });
 });
 
-app.listen(port,function(err){
+app.listen(port,ipaddress,function(err){
     console.log('running on server at port '+ port);
 })
